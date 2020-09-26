aa39b3871e4897a2e2aa4e5bfdb428c06e46320e
mariusz <vladyslaw@yahoo.com>
2020-09-26 14:31:42 +0200

    delete file from the server

--- src/service/DogService.kt
+++ src/service/DogService.kt
@@ -1,13 +1,20 @@
 package com.thernat.service
 
+import com.thernat.WEB_HOST_BASE_URL
 import com.thernat.repository.DatabaseFactory.dbQuery
 import com.thernat.repository.model.Dog
 import com.thernat.repository.table.Dogs
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.http.*
 import io.ktor.http.content.MultiPartData
 import io.ktor.http.content.PartData
 import io.ktor.http.content.forEachPart
 import io.ktor.http.content.streamProvider
 import io.ktor.util.KtorExperimentalAPI
+import io.ktor.utils.io.streams.*
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
@@ -23,8 +30,25 @@ class DogService {
         }
     }
 
-    suspend fun deleteDog(firebaseId: String, dogId: Int) = dbQuery {
-        Dogs.deleteWhere { Dogs.id eq dogId } > 0
+    suspend fun deleteDog(firebaseId: String, dogId: Int): Boolean {
+        val dogPicUrl = dbQuery {
+            Dogs.select { Dogs.id eq dogId}
+                    .map {
+                        it[Dogs.picUrl]
+                    }.first().substringAfterLast("/")
+        }
+        val client = HttpClient()
+        client.use {
+            val response: String = it.get("$WEB_HOST_BASE_URL/delete_dog_pic.php") {
+                parameter("uploader_id",firebaseId)
+                parameter("dog_pic_url",dogPicUrl)
+            }
+            print(response)
+        }
+        client.close()
+        return dbQuery {
+            Dogs.deleteWhere { Dogs.id eq dogId } > 0
+        }
     }
 
     private fun ResultRow.toDog() = Dog(
@@ -72,17 +96,43 @@ class DogService {
         }
         val uploaderId: String = uploader ?: return false
         val dogName: String = name ?: return false
-        dbQuery {
-            Dogs.insert { dog ->
-                dog[owner] = uploaderId
-                dog[sex] = ""
-                dog[description] = dogDescription
-                dog[picName] = dogName
-                dog[breed] = dogBreed
-                dog[picUrl] = "https://images.pexels.com/photos/4681107/pexels-photo-4681107.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+        tempFile?.let {
+            val fileName = "${uploaderId}_dog_${System.currentTimeMillis()}${it.name}"
+            HttpClient(Apache).use { client ->
+                val parts: List<PartData> = formData {
+                    val headersBuilder = HeadersBuilder()
+                    headersBuilder[HttpHeaders.ContentType] = "image"
+                    headersBuilder[HttpHeaders.ContentDisposition] = "filename=$fileName"
+                    this.append(
+                            "uploaded_file",
+                            InputProvider
+                            { it.inputStream().asInput() },
+                            headersBuilder.build()
+                    )
+                    this.append("uploader", uploaderId)
+                }
+                val result = (client.submitFormWithBinaryData<String>(formData = parts) {
+                    url("$WEB_HOST_BASE_URL/upload_dog_pic.php")
+
+                }.trim() == "true").also {
+                    tempFile?.delete()
+                }
+                if (result) {
+                    dbQuery {
+                        Dogs.insert { dog ->
+                            dog[owner] = uploaderId
+                            dog[sex] = ""
+                            dog[description] = dogDescription
+                            dog[picName] = dogName
+                            dog[breed] = dogBreed
+                            dog[picUrl] = "$WEB_HOST_BASE_URL/images/$uploaderId/dogs/$fileName"
+                        }
+                    }
+                    return true
+                }
             }
         }
-        return true
+        return false
     }
 }
 
